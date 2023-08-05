1cd45e6af755d7ef9dba19c73ec489e3b3ee7ed5
Joker941 <pro.julien.dussaut@gmail.com>
2023-08-05 23:20:06 +0200

    Ajout de l'event listener pour chaque div. Ajout d'une fonction générant un nombre aléatoire pour générer une couleur aléatoire. Ajout d'une fonction pour générer un float aléatoire pour générer une opacité aléatoire.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+function getRandomColor() {
+
+    return Math.floor(Math.random() * 256);
+}
+
+function getRandomOpacity() {
+    
+    return Math.round(Math.random() * 10) / 10;
+}
 function createDivs (nbrOflines) {
     let numberOflines = parseInt(nbrOflines) * parseInt(nbrOflines);
     
@@ -8,9 +17,25 @@ function createDivs (nbrOflines) {
     for (i = 1; i <= numberOflines; i++) {
         let div = document.createElement("div");
         div.classList.add("square_div");
+        div.classList.add("div_" + i);
         div.textContent = i;
         document.querySelector(".container_div").appendChild(div);
+
+        let divHover = document.querySelector(".div_" + i);
+        divHover.addEventListener("mouseover", function () {
+            divHover.style = "background-color: rgb(" + getRandomColor() + ", " + getRandomColor() + ", " + getRandomColor() + "); opacity: " + getRandomOpacity() + ";";
+        })
     }
 }
 
 createDivs(16);
+
+
+// let testHover = document.querySelector(".div_6");
+// testHover.addEventListener("mouseover", function () {
+//     testHover.style = "background-color: red;";
+//     //setTimeout(() => testHover.style = "background-color: aliceblue; opacity: .4", 3500);
+
+// })
+
+
